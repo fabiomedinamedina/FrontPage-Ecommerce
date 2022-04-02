a9f59049ab0b0bde636666f24d7ea963feec38a5
Fabio Medina <disenofabiomedina@gmail.com>
2022-04-02 10:58:27 -0500

    Fix: PopUp informacion

--- assents/js/script.js
+++ assents/js/script.js
@@ -78,6 +78,7 @@ const fillInfoProduct = ({name, price, compare_at_price}) => {
   const titleName = document.getElementById("title_product_name");
 
   titleName.innerHTML = name;
+  titleName.setAttribute('data-title', name);
   priceProduct.innerHTML = price.toLocaleString('es');
   priceProduct.setAttribute('data-price', price);
   priceTotal.innerHTML = `$ ${(priceProduct.getAttribute('data-price')*qtyInput.value).toLocaleString('es')}`;
@@ -149,20 +150,44 @@ const getValue = ({name, value})  => {
 //EVENTS CLICKS
 btnAddtoCart.addEventListener( "click", (e) => {
   e.preventDefault();
-  let atributos = {}
+  let atributos = [];
+  const tituloActual = document.getElementById("title_product_name").getAttribute("data-title");
+  const contentPopUp = document.getElementById("content_popup");
+  
   popUp.classList.remove("hidden_popup");
   attrColor.forEach((color) =>{
     if(color.checked){
-      atributos.color =  color.value;
+      const colorObj = {
+        name: color.name.charAt(0).toUpperCase()+color.name.substring(1),
+        value: color.value
+      }
+      atributos = [...atributos, colorObj];
     }
   })
   attrsize.forEach((size) =>{
     if(size.checked){
-      atributos.size =  size.value;
+      const sizeObj = {
+        name: size.name.charAt(0).toUpperCase()+size.name.substring(1),
+        value: size.value
+      }
+      atributos = [...atributos, sizeObj];
     }
   })
-
   console.log(atributos);
+  
+  let htmlOptions = '';
+  atributos.forEach((atributo) => {
+    htmlOptions +=
+    `<li>${atributo.name}: ${atributo.value}</li>
+    `;
+  })
+  const htmlPopUp =
+  `<h2>${tituloActual}</h2>
+  <ul>${htmlOptions}</ul>
+  `;
+
+  contentPopUp.innerHTML = htmlPopUp;
+  
   
 
 });
